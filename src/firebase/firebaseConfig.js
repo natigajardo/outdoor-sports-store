8bae197f60a6196cfc27bc815afbc817e5510509
Natalia Gajardo <natty.gajardo.s@gmail.com>
2021-09-20 15:11:57 -0300

    soluciones a errores

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -6,13 +6,6 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_FIREBASE_apiKey,
-  // authDomain: process.env.REACT_APP_FIREBASE_authDomain,
-  // projectId: process.env.REACT_APP_FIREBASE_projectId,
-  // storageBucket: process.env.REACT_APP_FIREBASE_storageBucket,
-  // messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId,
-  // appId: process.env.REACT_APP_FIREBASE_appId,
-
   apiKey: "AIzaSyDRACIxSl6bTy-8aNPwdk0JMMmhdCrmZ88",
   authDomain: "natgeo-db.firebaseapp.com",
   projectId: "natgeo-db",
